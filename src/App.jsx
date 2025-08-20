f481f163ff16feaa5130bb0b3c0191e12dc295eb
surya <nagasuryachennuri@gmail.com>
2025-08-20 21:24:15 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecommerce-surya">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
